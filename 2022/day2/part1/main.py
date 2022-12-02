e0cb7fcb5f2fb69c151d58b8308310cb191b5cdd
fedorareis <fedorareis@gmail.com>
2022-12-01 22:27:04 -0800

    add comments

--- 2022/day2/part1/main.py
+++ 2022/day2/part1/main.py
@@ -1,3 +1,17 @@
+# hints:
+#   "The first column is what your opponent is going to play:
+#    A for Rock, B for Paper, and C for Scissors. The second column--"
+# ---
+#   The second column, you reason, must be what you should play in response:
+#      X for Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious,
+#      so the responses must have been carefully chosen.
+#
+# Your total score is the sum of your scores for each round.
+# The score for a single round is the score for the shape you selected
+#    (1 for Rock, 2 for Paper, and 3 for Scissors)
+# plus the score for the outcome of the round
+#    (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
 with open('./input.txt') as f:
     result = 0
     decode = {
@@ -16,6 +30,8 @@ with open('./input.txt') as f:
         result += me
         if opp == me:
             result += 3
+        # my initial attempt is more readable but I decided to be clever and see if I could use math for determining the win conditions
+        # instead of directly have checks for the rock, paper, scissors win conditions
         elif opp - me == -1 or opp - me == 2:
             result += 6
 
--- 2022/day2/part2/main.py
+++ 2022/day2/part2/main.py
@@ -1,3 +1,16 @@
+# hints:
+#   "The first column is what your opponent is going to play:
+#    A for Rock, B for Paper, and C for Scissors. The second column--"
+# ---
+#   The second column, you learn, is the outcome of the round:
+#      X for Lose, Y for Draw, and Z for Win.
+#
+# Your total score is the sum of your scores for each round.
+# The score for a single round is the score for the shape you selected
+#    (1 for Rock, 2 for Paper, and 3 for Scissors)
+# plus the score for the outcome of the round
+#    (0 if you lost, 3 if the round was a draw, and 6 if you won).
+
 with open('./input.txt') as f:
     result = 0
     decode = {
@@ -17,10 +30,19 @@ with open('./input.txt') as f:
         outcome = parsed[1]
         result += outcomePoints[outcome]
         if outcome == 'Y':
+            # since this is a draw we can just use the value of the opponents selection
             result += opp
         elif outcome == 'Z':
+            # When you win the item you used is the next on the list, but you need to wrap around
+            # for rock to beat scissors. Each subsequent item on the list is 1 more than the value
+            # of the previous one. So if we mod the value first that solves our wrap around problem
+            # and then we can just add 1 to that.
             result += (opp % 3) + 1
         else:
-            result += opp - 1 if opp - 1 != 0 else 3
+            # Similar to winning we have a wrap around problem. This time in the other direction
+            # I'm probably being too clever here and should have stuck with my previous amath answer
+            # where I just subtract 1 and use a ternary so if that equals 0 it returns 3 instead
+            # but I really wanted to see if I could find a relatively simple equation that works
+            result += ((opp + 1) % 3) + 1
 
     print(result)
