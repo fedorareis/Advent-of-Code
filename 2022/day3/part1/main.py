6a1fade6899d745002e7f738ebb0656cdce3976d
fedorareis <fedorareis@gmail.com>
2022-12-03 01:35:37 -0800

    day 3 initial solution

--- 2022/day3/part1/main.py
+++ 2022/day3/part1/main.py
@@ -1,7 +1,22 @@
 with open('./input.txt') as f:
     result = 0
+    items = {}
     for line in f:
         line = line.strip()
-        parsed = line.split(' ')
+        idx = 0
+        for char in line:
+            if idx < len(line)/2:
+                items[char] = True
+            else:
+                if char in items:
+                    val = 0
+                    if char.isupper():
+                        val = ord(char) - 38
+                    else:
+                        val = ord(char) - 96
+                    result += val
+                    items = {}
+                    break
+            idx += 1
 
     print(result)
--- 2022/day3/part2/main.py
+++ 2022/day3/part2/main.py
@@ -1,7 +1,26 @@
 with open('./input.txt') as f:
     result = 0
+    items = {}
+    idx = 1
     for line in f:
         line = line.strip()
-        parsed = line.split(' ')
+        for char in line:
+            if idx == 1:
+                items[char] = False
+            elif idx == 2:
+                if char in items:
+                    items[char] = True
+            else:
+                if char in items and items[char]:
+                    val = 0
+                    if char.isupper():
+                        val = ord(char) - 38
+                    else:
+                        val = ord(char) - 96
+                    result += val
+                    items = {}
+                    idx = 0
+                    break
+        idx += 1
 
     print(result)
