7bd7d3ae140c5cfc15a97eb5bf1ab6bba0361fe5
fedorareis <fedorareis@gmail.com>
2022-12-04 22:57:46 -0800

    day 4 solution

--- 2022/day4/part1/main.py
+++ 2022/day4/part1/main.py
@@ -2,5 +2,11 @@ with open('./input.txt') as f:
     result = 0
     for line in f:
         line = line.strip()
-
+        pairs = line.split(",")
+        first = pairs[0].split("-")
+        second = pairs[1].split("-")
+        if int(first[0]) <= int(second[0]) and int(first[1]) >= int(second[1]):
+            result += 1
+        elif int(second[0]) <= int(first[0]) and int(second[1]) >= int(first[1]):
+            result += 1
     print(result)
--- 2022/day4/part2/main.py
+++ 2022/day4/part2/main.py
@@ -2,5 +2,9 @@ with open('./input.txt') as f:
     result = 0
     for line in f:
         line = line.strip()
-
+        pairs = line.split(",")
+        first = pairs[0].split("-")
+        second = pairs[1].split("-")
+        if (int(first[0]) <= int(second[0]) and int(second[0]) <= int(first[1])) or (int(second[0]) <= int(first[0]) and int(first[0]) <= int(second[1])):
+            result += 1
     print(result)
