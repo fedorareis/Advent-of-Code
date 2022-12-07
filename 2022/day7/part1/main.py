df507dde1ae1fbe4b03615dfc766cec9d6a0c1ec
fedorareis <fedorareis@gmail.com>
2022-12-06 22:24:33 -0800

    day 7 results

--- 2022/day7/part1/main.py
+++ 2022/day7/part1/main.py
@@ -1,5 +1,33 @@
 with open('./input.txt') as f:
     result = 0
+    dirs = {}
+    curr_dirs = [""]
     for line in f:
+        line = line.strip()
+        vals = line.split(" ")
+        if vals[0] == "$":
+            if vals[1] == "cd":
+                if vals[2] == "..":
+                    curr_dirs.pop()
+                elif vals[2] == "/":
+                    curr_dirs = [""]
+                else:
+                    curr_dirs.append(vals[2])
+
+        else:
+            if vals[0].isnumeric():
+                size = int(vals[0])
+                temp = curr_dirs.copy()
+                while len(temp) > 0:
+                    key = "/".join(temp)
+                    if key in dirs:
+                        dirs[key] += size
+                    else:
+                        dirs[key] = size
+                    temp.pop()
+
+    for d, s in dirs.items():
+        if s <= 100000:
+            result += s
 
     print(result)
--- 2022/day7/part2/main.py
+++ 2022/day7/part2/main.py
@@ -1,5 +1,35 @@
 with open('./input.txt') as f:
-    result = 0
+    filesystem = 70000000
+    result = filesystem
+    dirs = {}
+    curr_dirs = [""]
     for line in f:
+        line = line.strip()
+        vals = line.split(" ")
+        if vals[0] == "$":
+            if vals[1] == "cd":
+                if vals[2] == "..":
+                    curr_dirs.pop()
+                elif vals[2] == "/":
+                    curr_dirs = [""]
+                else:
+                    curr_dirs.append(vals[2])
+
+        else:
+            if vals[0].isnumeric():
+                size = int(vals[0])
+                temp = curr_dirs.copy()
+                while len(temp) > 0:
+                    key = "/".join(temp)
+                    if key in dirs:
+                        dirs[key] += size
+                    else:
+                        dirs[key] = size
+                    temp.pop()
+
+    remaining = filesystem - dirs[""]
+    for d, s in dirs.items():
+        if s + remaining >= 30000000 and s < result:
+            result = s
 
     print(result)
