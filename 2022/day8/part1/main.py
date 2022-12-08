f8d8a3b531fd380d4e2b6e2e77c596988a7d6100
fedorareis <fedorareis@gmail.com>
2022-12-07 22:35:43 -0800

    day 8 solution

--- 2022/day8/part1/main.py
+++ 2022/day8/part1/main.py
@@ -1,6 +1,59 @@
+def visible(forest, row, col):
+    isVisible = True
+    height = int(forest[row][col])
+    decX = col - 1
+    incX = col + 1
+    decY = row - 1
+    incY = row + 1
+
+    while decX >= 0 and isVisible:
+        if height <= int(forest[row][decX]):
+            isVisible = False
+        decX += -1
+    if isVisible:
+        return isVisible
+
+    isVisible = True
+    while incX <= len(forest) - 1 and isVisible:
+        if height <= int(forest[row][incX]):
+            isVisible = False
+        incX += 1
+    if isVisible:
+        return isVisible
+
+    isVisible = True
+    while decY >= 0 and isVisible:
+        if height <= int(forest[decY][col]):
+            isVisible = False
+        decY += -1
+    if isVisible:
+        return isVisible
+
+    isVisible = True
+    while incY <= len(forest) - 1 and isVisible:
+        if height <= int(forest[incY][col]):
+            isVisible = False
+        incY += 1
+
+    return isVisible
+
+
 with open('./input.txt') as f:
     result = 0
+    forest = []
     for line in f:
         line = line.strip()
+        forest.append(list(line))
+
+    y = 0
+    for row in forest:
+        x = 0
+        for col in row:
+            isVisible = visible(forest, y, x)
+            # print(str(y) + ',' + str(x) + ': ' + str(isVisible))
+            if isVisible:
+                result += 1
+            x += 1
+        y += 1
 
     print(result)
--- 2022/day8/part2/main.py
+++ 2022/day8/part2/main.py
@@ -1,6 +1,66 @@
+def scenic(forest, row, col):
+    isVisible = True
+    height = int(forest[row][col])
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+    decX = col - 1
+    incX = col + 1
+    decY = row - 1
+    incY = row + 1
+
+    while decX >= 0 and isVisible:
+        left += 1
+        if height <= int(forest[row][decX]):
+            isVisible = False
+        decX += -1
+    if left == 0:
+        return left
+
+    isVisible = True
+    while incX <= len(forest) - 1 and isVisible:
+        right += 1
+        if height <= int(forest[row][incX]):
+            isVisible = False
+        incX += 1
+    if right == 0:
+        return right
+
+    isVisible = True
+    while decY >= 0 and isVisible:
+        down += 1
+        if height <= int(forest[decY][col]):
+            isVisible = False
+        decY += -1
+    if down == 0:
+        return down
+
+    isVisible = True
+    while incY <= len(forest) - 1 and isVisible:
+        up += 1
+        if height <= int(forest[incY][col]):
+            isVisible = False
+        incY += 1
+
+    return up * down * left * right
+
+
 with open('./input.txt') as f:
     result = 0
+    forest = []
     for line in f:
         line = line.strip()
+        forest.append(list(line))
+
+    y = 0
+    for row in forest:
+        x = 0
+        for col in row:
+            score = scenic(forest, y, x)
+            if score > result:
+                result = score
+            x += 1
+        y += 1
 
     print(result)
