a50354ecf8ef635bb3841d717c403ba2ea08e817
fedorareis <fedorareis@gmail.com>
2022-12-09 21:49:26 -0800

    day 10 solution

--- 2022/day10/part1/main.py
+++ 2022/day10/part1/main.py
@@ -1,6 +1,24 @@
 with open('./input.txt') as f:
+    register = 1
+    cycleTotal = 1
     result = 0
     for line in f:
         line = line.strip()
+        ops = line.split(" ")
+        cycles = 0
+        val = 0
+        if ops[0] == "noop":
+            cycles = 1
+        elif ops[0] == "addx":
+            cycles = 2
+            val = int(ops[1])
+
+        while cycles > 0:
+            if cycleTotal == 20 or (cycleTotal - 20) % 40 == 0:
+                result += (register * cycleTotal)
+            cycles += -1
+            cycleTotal += 1
+
+        register += val
 
     print(result)
--- 2022/day10/part2/main.py
+++ 2022/day10/part2/main.py
@@ -1,6 +1,31 @@
 with open('./input.txt') as f:
-    result = 0
+    register = 1
+    cycleTotal = 1
+    result = ""
+    scan = 0
     for line in f:
         line = line.strip()
+        ops = line.split(" ")
+        cycles = 0
+        val = 0
+        if ops[0] == "noop":
+            cycles = 1
+        elif ops[0] == "addx":
+            cycles = 2
+            val = int(ops[1])
 
-    print(result)
+        while cycles > 0:
+            if scan >= register - 1 and scan <= register + 1:
+                result += "#"
+            else:
+                result += "."
+
+            scan += 1
+            if cycleTotal % 40 == 0:
+                print(result)
+                result = ""
+                scan = 0
+            cycles += -1
+            cycleTotal += 1
+
+        register += val
