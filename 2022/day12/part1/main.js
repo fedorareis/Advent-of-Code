aec802e027838ad9173385652376322f6da048af
fedorareis <fedorareis@gmail.com>
2022-12-11 14:46:25 -0800

    day 12 js prep

---
+++ 2022/day12/part1/main.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+let result = 0;
+let input = [];
+
+try {
+  input = fs
+    .readFileSync("input.txt", "utf8")
+    .split("\n")
+    .map((line) => line.trim());
+} catch (err) {
+  console.error(err);
+}
+
+console.log(result);
