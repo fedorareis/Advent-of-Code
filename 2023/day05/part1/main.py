185ab2544696d7b4c02c1de78aa6fdd5ff9df395
fedorareis <fedorareis@gmail.com>
2023-12-03 21:52:47 -0800

    day 5 setup

---
+++ 2023/day05/part1/main.py
@@ -0,0 +1,10 @@
+with open('./input.txt') as f:
+    result = 0
+
+    for line in f:
+        line = line.strip()
+        
+        for val in line:
+            result += val
+
+    print(result)
