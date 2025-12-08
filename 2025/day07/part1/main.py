d740682322315c50e52d07bcf71d62bf54c2897a
fedorareis <fedorareis@gmail.com>
2025-12-07 17:59:42 -0800

    day 7 solution (maybe)

--- 2025/day07/part1/main.py
+++ 2025/day07/part1/main.py
@@ -1,18 +1,24 @@
 with open("./input.txt") as f:
     result = 0
-    ranges = []
-    ranges_done = False
+    prev = ""
     for line in f:
         line = line.strip()
-        if line == "":
-            ranges_done = True
-        elif not ranges_done:
-            ranges.append([int(x) for x in line.split("-")])
-        else:
-            val = int(line)
-            for range in ranges:
-                if val >= range[0] and val <= range[1]:
+        if prev == "":
+            prev = line
+            continue
+
+        line = list(line)
+        for i in range(len(line)):
+            if prev[i] == "S":
+                line[i] = "|"
+
+            if prev[i] == "|":
+                if line[i] == "^":
+                    line[i - 1] = "|"
+                    line[i + 1] = "|"
                     result += 1
-                    break
+                else:
+                    line[i] = "|"
+        prev = "".join(line)
 
     print(result)
--- 2025/day07/part2/main.py
+++ 2025/day07/part2/main.py
@@ -1,47 +1,84 @@
+from pprint import pprint
+
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def traverse(root, routes):
+    if root is None:
+        return routes + 1
+    # print(root.val)
+
+    routes = traverse(root.left, routes)
+    return traverse(root.right, routes)
+
+
 with open("./input.txt") as f:
     result = 0
-    ranges = []
+    map = []
+    lookup = {}
+    prev = ""
+    row = -1
     for line in f:
+        row += 1
         line = line.strip()
-        if line == "":
-            for val in ranges:
-                result += val[1] - val[0] + 1
-            break
-        else:
-            temp = [int(x) for x in line.split("-")]
-            end = False
-            count = 0
-            while not end:
-                expand = False
-                for i in range(len(ranges)):
-                    val = ranges[i]
-                    # equal
-                    if temp[0] >= val[0] and temp[1] <= val[1]:
-                        temp = ranges.pop(i)
-                        break
-                    # replace
-                    if temp[0] <= val[0] and temp[1] >= val[1]:
-                        expand = True
-                        val[0] = temp[0]
-                        val[1] = temp[1]
-                        temp = ranges.pop(i)
-                        break
-                    # exapnd upper bound
-                    if temp[0] >= val[0] and temp[0] <= val[1]:
-                        expand = True
-                        if temp[1] > val[1]:
-                            val[1] = temp[1]
-                        temp = ranges.pop(i)
-                        break
-                    # expand lower bound
-                    if temp[1] >= val[0] and temp[1] <= val[1]:
-                        expand = True
-                        if temp[0] < val[0]:
-                            val[0] = temp[0]
-                        temp = ranges.pop(i)
-                        break
-                if not expand:
-                    ranges.append(temp)
-                    end = True
+        if prev == "":
+            prev = line
+            map.append(list(line))
+            continue
+
+        line = list(line)
+        for i in range(len(line)):
+            if prev[i] == "S":
+                lookup["S"] = Node(str(row - 1) + "," + str(i))
+                line[i] = "|"
+
+            if prev[i] == "|":
+                if line[i] == "^":
+                    currentNode = Node(str(row) + "," + str(i))
+                    line[i - 1] = "|"
+                    line[i + 1] = "|"
+                    for j in range(row - 1, -1, -1):
+                        if map[j][i] == "." or map[j][i] == "^":
+                            break
+                        if map[j][i] == "S":
+                            lookup["S"] = currentNode
+                            break
+                        if (
+                            i > 0
+                            and map[j][i - 1] == "^"
+                            and str(j) + "," + str(i - 1) in lookup
+                        ):
+                            lookup[str(j) + "," + str(i - 1)].right = currentNode
+                        if (
+                            i < len(line)
+                            and map[j][i + 1] == "^"
+                            and str(j) + "," + str(i + 1) in lookup
+                        ):
+                            lookup[str(j) + "," + str(i + 1)].left = currentNode
+                    lookup[str(row) + "," + str(i)] = currentNode
+                else:
+                    line[i] = "|"
+        prev = "".join(line)
+        map.append(line)
+    # pprint(map)
+    print("finished building tree")
+    result = traverse(lookup["S"], 0)
 
     print(result)
+
+# for i in range(len(line)):
+#         if prev[i] == "S":
+#             line[i] = "|"
+
+#         if prev[i] == "|":
+#             if line[i] == "^":
+#                 line[i - 1] = "|"
+#                 line[i + 1] = "|"
+#                 result += 1
+#             else:
+#                 line[i] = "|"
